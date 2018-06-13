cbe4b5ae1bfd23b77b0b8798ae9b00da59d3fc79
Rivera <J.C.RIVERA@cgiar.org>
2018-06-13 15:37:08 -0500

    box plot

--- DIA_1/Ejercicio_1.R
+++ DIA_1/Ejercicio_1.R
@@ -76,7 +76,7 @@ summary(datos_mora$PrecAcc_2)
 summary(datos_mora$trmm_3)
 
 #2.6 Graficas barras
-boxplot(datos_mora$trmm_3, main="Box Plot de Preciptacion del tercer mes antes de cosecha", col= "blue")
+boxplot(datos_mora$trmm_3, main="Box plot de preciptacion del tercer mes antes de cosecha", col= "blue")
 
 
 
